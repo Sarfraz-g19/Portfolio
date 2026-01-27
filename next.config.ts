3a79f7098eaab78849796b27fafc8c857a24193a
Sarfraz-g19 <mohammadsarfraj2001@gmail.com>
2026-01-27 14:50:46 -0800

    Vercel Force Rebuild: Navbar Icons Update

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 
-// Forced Rebuild: 2026-01-27 14:33
+// Forced Rebuild: 2026-01-27 14:50 (Navbar GitHub Icon)
 
 const nextConfig: NextConfig = {
   /* config options here */
